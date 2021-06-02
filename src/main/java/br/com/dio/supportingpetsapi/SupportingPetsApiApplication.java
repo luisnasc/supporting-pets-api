39569acf59340e6a77cc053df8b0de6c84e30aaa
luisnasc <luisbrunu@gmail.com>
2021-06-02 04:24:46 -0300

    Upload the java project

---
+++ src/main/java/br/com/dio/supportingpetsapi/SupportingPetsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.supportingpetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportingPetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportingPetsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/supportingpetsapi/SupportingPetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.supportingpetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportingPetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
