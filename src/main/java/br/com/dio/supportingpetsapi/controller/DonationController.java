a4ac853a7051c83bb4caa676615dbe3ea7c27899
luisnasc <luisbrunu@gmail.com>
2021-06-03 04:50:43 -0300

    Develop operations delete and update Donation

--- src/main/java/br/com/dio/supportingpetsapi/controller/DonationController.java
+++ src/main/java/br/com/dio/supportingpetsapi/controller/DonationController.java
@@ -6,8 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dio.supportingpetsapi.dto.MessageResponseDTO;
 import br.com.dio.supportingpetsapi.dto.request.DonationDTO;
+import br.com.dio.supportingpetsapi.exception.DonationNotFoundException;
 import br.com.dio.supportingpetsapi.service.DonationService;
 import lombok.AllArgsConstructor;
 
@@ -37,4 +41,19 @@ public class DonationController {
 		
 	}
 	
+	@GetMapping("/{id}")
+	public DonationDTO findById(@PathVariable Long id) throws DonationNotFoundException {
+		return donationService.findById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid DonationDTO donationDTO) throws DonationNotFoundException {
+		return donationService.updateById(id, donationDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) throws DonationNotFoundException {
+		donationService.deleteById(id);
+	}
 }
--- src/main/java/br/com/dio/supportingpetsapi/service/DonationService.java
+++ src/main/java/br/com/dio/supportingpetsapi/service/DonationService.java
@@ -2,18 +2,13 @@ package br.com.dio.supportingpetsapi.service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import br.com.dio.supportingpetsapi.dto.MessageResponseDTO;
 import br.com.dio.supportingpetsapi.dto.request.DonationDTO;
-import br.com.dio.supportingpetsapi.entity.Animal;
 import br.com.dio.supportingpetsapi.entity.Donation;
-import br.com.dio.supportingpetsapi.exception.AnimalNotFoundException;
 import br.com.dio.supportingpetsapi.exception.DonationNotFoundException;
 import br.com.dio.supportingpetsapi.mapper.DonationMapper;
 import br.com.dio.supportingpetsapi.repository.DonationRepository;
@@ -30,19 +25,38 @@ public class DonationService {
 		this.donationRepository = donationRepository;
 	}
 
-	public List<DonationDTO> findAll() {
-		return donationRepository.findAll()
-				.stream()
-				.map(donationMapper::toDTO)
-				.collect(Collectors.toList());
-				
-	}
-
 	public MessageResponseDTO create(@RequestBody @Valid DonationDTO donationDTO) {
 		Donation donationToSave = donationMapper.toModel(donationDTO);
 		Donation savedDonation = donationRepository.save(donationToSave);
 		
 		return createMessageResponse(savedDonation.getIdDonation(), "Donation created with ID ");
+	}
+	
+	public List<DonationDTO> findAll() {
+		return donationRepository.findAll()
+				.stream()
+				.map(donationMapper::toDTO)
+				.collect(Collectors.toList());
+	}
+	
+	public DonationDTO findById(Long id) throws DonationNotFoundException {
+		Donation donation = verifyIfExists(id);
+		return donationMapper.toDTO(donation);
+	}
+
+
+	public MessageResponseDTO updateById(Long id, @Valid DonationDTO donationDTO) throws DonationNotFoundException {
+		verifyIfExists(id);
+		Donation donationToUpdate = donationMapper.toModel(donationDTO);
+		Donation updatedDonation = donationRepository.save(donationToUpdate);
+		
+		return createMessageResponse(updatedDonation.getIdDonation(), "Donation updated with ID ");
+	}
+
+	public void deleteById(Long id) throws DonationNotFoundException {
+		verifyIfExists(id);
+		
+		donationRepository.deleteById(id);
 	}
 	
 	private MessageResponseDTO createMessageResponse(Long idDonation, String message) {
